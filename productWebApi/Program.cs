525c0e083289c0cb6f679d05d3080448b60a9e3b
javiergalvanmancera <javier.galvan.mancera@gmail.com>
2026-04-29 15:32:32 -0500

    swwagger a pord

--- productWebApi/Program.cs
+++ productWebApi/Program.cs
@@ -27,11 +27,11 @@ ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConne
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
